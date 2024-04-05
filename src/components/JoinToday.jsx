b03f5fa40c0eafceea24e9374186ab97f6fa91b2
amora7741 <mora7741@gmail.com>
2024-04-05 15:49:07 -0700

    Add signup term links

--- src/components/JoinToday.jsx
+++ src/components/JoinToday.jsx
@@ -1,18 +1,22 @@
 import Button from './Button';
 
 const JoinToday = () => {
+  const futureImplement = () => {
+    alert('Not implemented yet :(');
+  };
+
   return (
     <div className='jointoday'>
       <h2>Join today.</h2>
       <div className='jointoday-signup'>
-        <button>
+        <button onClick={() => futureImplement()}>
           <img
             src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg'
             className='button-icon'
           />
           Sign up with Google
         </button>
-        <button>
+        <button onClick={() => futureImplement()}>
           <img
             src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/apple/apple-original.svg'
             className='button-icon'
@@ -23,9 +27,20 @@ const JoinToday = () => {
         <Button backgroundColor='rgb(29, 155, 240)' textColor='white'>
           Create account
         </Button>
-        <p>
-          By signing up, you agree to the Terms of Service and Privacy Policy,
-          including Cookie Use.
+        <p className='signup-terms'>
+          By signing up, you agree to the{' '}
+          <span>
+            <a href=''>Terms of Service</a>
+          </span>{' '}
+          and{' '}
+          <span>
+            <a href=''>Privacy Policy</a>
+          </span>
+          , including{' '}
+          <span>
+            <a href=''>Cookie Use</a>
+          </span>
+          .
         </p>
       </div>
     </div>
