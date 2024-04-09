26f014a13d5747571d844fc392c46d82177e3721
amora7741 <mora7741@gmail.com>
2024-04-08 17:07:55 -0700

    Add create account inputs

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,8 +1,25 @@
 const CreateAccount = () => {
   return (
     <div className='createaccount-container'>
-      <h1>Hello</h1>
-      <h1>Create ur account :3</h1>
+      <h1>Create your account</h1>
+      <form action=''>
+        <div className='fieldholder'>
+          <input type='text' required name='name' id='name' />
+          <label htmlFor='name'>Name</label>
+        </div>
+        <div className='fieldholder'>
+          <input type='email' required name='email' id='email' />
+          <label htmlFor='email'>Email</label>
+        </div>
+        <div>
+          <h4>Date of birth</h4>
+          <p className='gray'>
+            This will not be shown publicly. Confirm your own age, even if this
+            account is for a business, a pet, or something else.
+          </p>
+        </div>
+        <div className='birthday-container'></div>
+      </form>
     </div>
   );
 };
