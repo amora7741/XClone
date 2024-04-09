cc39ca7a6f756469562d3952b5fbf6d3bc95a693
amora7741 <mora7741@gmail.com>
2024-04-09 15:53:25 -0700

    Setup second page of create account modal

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Button from './Button';
 
 const CreateAccount = () => {
+  const [secondPage, setSecondPage] = useState(false);
   const [birthMonth, setBirthMonth] = useState('');
   const [birthDay, setBirthDay] = useState('');
   const [birthYear, setBirthYear] = useState('');
@@ -36,79 +37,102 @@ const CreateAccount = () => {
 
   return (
     <div className='createaccount-container'>
-      <h1>Create your account</h1>
       <form action=''>
-        <div className='fieldholder'>
-          <input type='text' required name='name' id='name' />
-          <label htmlFor='name'>Name</label>
-        </div>
-        <div className='fieldholder'>
-          <input type='email' required name='email' id='email' />
-          <label htmlFor='email'>Email</label>
-        </div>
-        <div>
-          <h4>Date of birth</h4>
-          <p className='gray'>
-            This will not be shown publicly. Confirm your own age, even if this
-            account is for a business, a pet, or something else.
-          </p>
-        </div>
-        <div className='birthday-container'>
-          <div className='fieldholder'>
-            <select
-              name='birthMonth'
-              id='birthMonth'
-              value={birthMonth}
-              onChange={(e) => setBirthMonth(e.target.value)}
+        {!secondPage && (
+          <>
+            <h1>Create your account</h1>
+            <div className='fieldholder'>
+              <input type='text' required name='name' id='name' />
+              <label htmlFor='name'>Name</label>
+            </div>
+            <div className='fieldholder'>
+              <input type='email' required name='email' id='email' />
+              <label htmlFor='email'>Email</label>
+            </div>
+            <div>
+              <h4>Date of birth</h4>
+              <p className='gray'>
+                This will not be shown publicly. Confirm your own age, even if
+                this account is for a business, a pet, or something else.
+              </p>
+            </div>
+            <div className='birthday-container'>
+              <div className='fieldholder'>
+                <select
+                  name='birthMonth'
+                  id='birthMonth'
+                  value={birthMonth}
+                  onChange={(e) => setBirthMonth(e.target.value)}
+                >
+                  <option value=''></option>
+                  {Array.from({ length: 12 }, (_, index) => (
+                    <option key={index} value={index + 1}>
+                      {new Date(0, index).toLocaleString('default', {
+                        month: 'long',
+                      })}
+                    </option>
+                  ))}
+                </select>
+                <label htmlFor='birthMonth'>Month</label>
+              </div>
+              <div className='fieldholder'>
+                <select
+                  name='birthDay'
+                  id='birthDay'
+                  value={birthDay}
+                  onChange={(e) => setBirthDay(e.target.value)}
+                >
+                  <option value=''></option>
+                  {daysInMonth.map((day) => (
+                    <option key={day} value={day}>
+                      {day}
+                    </option>
+                  ))}
+                </select>
+                <label htmlFor='birthDay'>Day</label>
+              </div>
+              <div className='fieldholder'>
+                <select
+                  name='birthYear'
+                  id='birthYear'
+                  value={birthYear}
+                  onChange={(e) => setBirthYear(e.target.value)}
+                >
+                  <option value=''></option>
+                  {years.map((year) => (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
+                </select>
+                <label htmlFor='birthYear'>Year</label>
+              </div>
+            </div>
+            <Button
+              backgroundColor='white'
+              textColor='black'
+              onClick={() => setSecondPage(true)}
             >
-              <option value=''></option>
-              {Array.from({ length: 12 }, (_, index) => (
-                <option key={index} value={index + 1}>
-                  {new Date(0, index).toLocaleString('default', {
-                    month: 'long',
-                  })}
-                </option>
-              ))}
-            </select>
-            <label htmlFor='birthMonth'>Month</label>
-          </div>
-          <div className='fieldholder'>
-            <select
-              name='birthDay'
-              id='birthDay'
-              value={birthDay}
-              onChange={(e) => setBirthDay(e.target.value)}
-            >
-              <option value=''></option>
-              {daysInMonth.map((day) => (
-                <option key={day} value={day}>
-                  {day}
-                </option>
-              ))}
-            </select>
-            <label htmlFor='birthDay'>Day</label>
-          </div>
-          <div className='fieldholder'>
-            <select
-              name='birthYear'
-              id='birthYear'
-              value={birthYear}
-              onChange={(e) => setBirthYear(e.target.value)}
-            >
-              <option value=''></option>
-              {years.map((year) => (
-                <option key={year} value={year}>
-                  {year}
-                </option>
-              ))}
-            </select>
-            <label htmlFor='birthYear'>Year</label>
-          </div>
-        </div>
+              Next
+            </Button>
+          </>
+        )}
+
+        {secondPage && (
+          <>
+            <div>
+              <h1>What should we call you?</h1>
+              <p className='gray'>
+                Your @username is unique. You can always change it later.
+              </p>
+            </div>
+            <div className='fieldholder'>
+              <input type='text' name='username' id='username' required />
+              <label htmlFor='username'>Username</label>
+            </div>
+          </>
+        )}
       </form>
-      <Button backgroundColor='white' textColor='black'>
-        Next
-      </Button>
     </div>
   );
 };
