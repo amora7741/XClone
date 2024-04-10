ffabbcfb766deb367ebe067847aaabd7b17f76ca
amora7741 <mora7741@gmail.com>
2024-04-10 16:39:39 -0700

    Setup page for entering login password

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Button from './Button';
 
 const SignIn = ({ onClick }) => {
@@ -5,57 +6,112 @@ const SignIn = ({ onClick }) => {
     alert('Not implemented yet :(');
   };
 
+  const [loginCredentials, setLoginCredentials] = useState('');
+  const [password, setPassword] = useState('');
+  const [secondPage, setSecondPage] = useState(false);
+
   return (
     <div className='signin-container'>
-      <h2>Sign in to X</h2>
-      <div className='signin-form'>
-        <Button
-          backgroundColor='white'
-          textColor='black'
-          onClick={notImplemented}
-        >
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg'
-            className='button-icon'
-          />
-          Sign in with Google
-        </Button>
-        <Button
-          backgroundColor='white'
-          textColor='black'
-          onClick={notImplemented}
-        >
-          <img
-            src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/apple/apple-original.svg'
-            className='button-icon'
-          />
-          Sign in with Apple
-        </Button>
-        <p className='separator'>or</p>
-        <form action=''>
-          <div className='fieldholder'>
-            <input name='login' id='login' required></input>
-            <label htmlFor='login'>Phone, email, or username</label>
+      {!secondPage && (
+        <>
+          <h2>Sign in to X</h2>
+          <div className='signin-form'>
+            <Button
+              backgroundColor='white'
+              textColor='black'
+              onClick={notImplemented}
+            >
+              <img
+                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg'
+                className='button-icon'
+              />
+              Sign in with Google
+            </Button>
+            <Button
+              backgroundColor='white'
+              textColor='black'
+              onClick={notImplemented}
+            >
+              <img
+                src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/apple/apple-original.svg'
+                className='button-icon'
+              />
+              Sign in with Apple
+            </Button>
+            <p className='separator'>or</p>
+            <form>
+              <div className='fieldholder'>
+                <input
+                  name='login'
+                  id='login'
+                  required
+                  value={loginCredentials}
+                  onChange={(e) => setLoginCredentials(e.target.value)}
+                />
+                <label htmlFor='login'>Phone, email, or username</label>
+              </div>
+            </form>
+            <Button
+              backgroundColor='white'
+              textColor='black'
+              onClick={() => setSecondPage(true)}
+            >
+              Next
+            </Button>
+            <Button
+              backgroundColor='transparent'
+              textColor='white'
+              borderColor='rgb(113, 118, 123)'
+              onClick={notImplemented}
+            >
+              Forgot password?
+            </Button>
           </div>
-        </form>
-        <Button backgroundColor='white' textColor='black'>
-          Next
-        </Button>
-        <Button
-          backgroundColor='transparent'
-          textColor='white'
-          borderColor='rgb(113, 118, 123)'
-          onClick={notImplemented}
-        >
-          Forgot password?
-        </Button>
-      </div>
-      <p className='gray'>
-        Don't have an account?{' '}
-        <span role='button' onClick={onClick}>
-          Sign up
-        </span>
-      </p>
+          <p className='gray'>
+            Don't have an account?{' '}
+            <span role='button' onClick={onClick}>
+              Sign up
+            </span>
+          </p>
+        </>
+      )}
+
+      {secondPage && (
+        <>
+          <h2>Enter your password</h2>
+          <form className='signin-form second'>
+            <div className='fieldholder'>
+              <input
+                name='login'
+                id='login'
+                required
+                value={loginCredentials}
+                disabled
+              />
+              <label htmlFor='login'>Phone, email, or username</label>
+            </div>
+            <div className='fieldholder'>
+              <input
+                name='password'
+                id='password'
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor='password'>Password</label>
+            </div>
+            <Button backgroundColor='white' textColor='black' type='submit'>
+              Log in
+            </Button>
+            <p className='gray'>
+              Don't have an account?{' '}
+              <span role='button' onClick={onClick}>
+                Sign up
+              </span>
+            </p>
+          </form>
+        </>
+      )}
     </div>
   );
 };
