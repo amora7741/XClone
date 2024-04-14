e3f837337ef5b5f76678dc12817ea689af44c87b
amora7741 <mora7741@gmail.com>
2024-04-13 17:56:11 -0700

    Add homepage items

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import NavbarItems from './NavbarItems';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavbarItems />
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   const { user } = useContext(AuthContext);
   return (
     <main>
       <h1>Home Page</h1>
-      {user && (
-        <>
-          <h2>{user.username}</h2>
-        </>
-      )}
+      <Navbar />
     </main>
   );
 };
