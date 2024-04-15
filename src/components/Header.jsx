efee33d8ff86e99d7865278dc01b12599b11f552
amora7741 <mora7741@gmail.com>
2024-04-15 16:06:45 -0700

    Setup tweet modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
-import Navbar from '../components/Navbar';
-import Button from '../components/Button';
+import Navbar from './Navbar';
+import Button from './Button';
 import Icon from './Icon';
+import { useState } from 'react';
 
 const Header = () => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-    <header>
-      <Icon classname='xicon small' />
-      <Navbar />
-      <Button backgroundColor='rgb(29, 155, 240)' textColor='white'>
-        Post
-      </Button>
-    </header>
+    <>
+      <header>
+        <Icon classname='xicon small' />
+        <Navbar />
+        <Button backgroundColor='rgb(29, 155, 240)' textColor='white'>
+          Post
+        </Button>
+        <Button backgroundColor='rgb(29, 155, 240)' textColor='white'>
+          Post
+        </Button>
+      </header>
+    </>
   );
 };
 
