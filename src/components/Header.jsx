107e836a1479bca1adf08ce42a48301de901bb60
amora7741 <mora7741@gmail.com>
2024-04-16 14:36:59 -0700

    Setup user profile in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,39 @@
 import Navbar from './Navbar';
 import Button from './Button';
 import Icon from './Icon';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import TweetModal from './TweetModal';
+import { AuthContext } from '../context/AuthContext';
 
 const Header = () => {
+  const { user } = useContext(AuthContext);
   const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
       <header>
-        <Link to='/home' className='xicon-home'>
-          <Icon classname='xicon small' />
-        </Link>
-        <Navbar />
-        <Button
-          backgroundColor='rgb(29, 155, 240)'
-          textColor='white'
-          onClick={() => setModalOpen(true)}
-        >
-          Post
-        </Button>
+        <div className='header-top'>
+          <Link to='/home' className='xicon-home'>
+            <Icon classname='xicon small' />
+          </Link>
+          <Navbar />
+          <Button
+            backgroundColor='rgb(29, 155, 240)'
+            textColor='white'
+            onClick={() => setModalOpen(true)}
+          >
+            Post
+          </Button>
+        </div>
+        <div className='header-profile'>
+          <div className='header-profile-left'>
+            <div className='picture'></div>
+            <div className='profile-info'>
+              <h5>{user.name}</h5>
+              <p>@{user.username}</p>
+            </div>
+          </div>
+        </div>
       </header>
       {modalOpen && (
         <TweetModal open={modalOpen} onClose={() => setModalOpen(false)} />
